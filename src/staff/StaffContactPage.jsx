92c972fff9f7148b597589a596af6af0465049cc
Zahid Jamali <zahid.jamali4848@gmail.com>
2026-02-17 12:30:54 +0500

    Add contact issue is resolved

--- src/staff/StaffContactPage.jsx
+++ src/staff/StaffContactPage.jsx
@@ -49,7 +49,7 @@ const StaffContactsPage = () => {
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold text-red-500">Contacts</h1>
         <button
-          onClick={() => setShowModal(true)}
+          onClick={() => setShowModal("Add")}
           className="bg-red-600 hover:bg-red-700 px-4 py-2 rounded text-sm font-semibold"
         >
           + Add Contact
